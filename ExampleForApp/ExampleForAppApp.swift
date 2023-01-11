80f3286cb9b700da2b3c64cb89b6e4fd1fe4d978
irinadeeva <51794769+irinadeeva@users.noreply.github.com>
2023-01-11 10:48:31 +0100

    Initial Commit

---
+++ ExampleForApp/ExampleForAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleForAppApp.swift
+//  ExampleForApp
+//
+//  Created by Irina Deeva on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleForAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
